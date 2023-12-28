7c4cad0917a9ac414938f6ac44ce7de1cb22f501
Overionised <153439700+Overionised@users.noreply.github.com>
2023-12-28 20:32:32 +0100

    not much yet

--- battleships/main.cpp
+++ battleships/main.cpp
@@ -5,7 +5,22 @@ using namespace std;
 
 int main()
 {    
+    char firstinput;
     logo();
+    cout<<endl<<endl<<endl;
+
+    cout<<"welcome to BATTLESHIPS"<<endl;
+    cout<<"For rules and actions type h"<<endl;
+
+    cin>>firstinput;
+    switch (firstinput){
+
+        case 'h':
+            cout<<"you chose help, someone should probably wite some options here"<<endl;
+            break;
+        case 'v':
+            visualise(arena_p1);
+    }
     
-    visualise(arena_p1);
+    return 0;
 }
