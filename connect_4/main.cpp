15f616c20a9aeb42d0ed165e12ad6ba81a55ae24
Overionised <153439700+Overionised@users.noreply.github.com>
2024-01-13 13:45:43 +0100

    Update main.cpp

--- connect_4/main.cpp
+++ connect_4/main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <unistd.h>
+#include <stdlib.h>
 #include "functions.cpp"
 
 int main(){
     bool player = true;
-	bool win = false;
-	int coordinatex;
+    bool win = false;
+    int coordinatex;
 
     while(win == false){
         coordinatex = user_input();
         player = shoot(player, coordinatex);
+        win = connected4(player, coordinatex, win);
     }
 }
